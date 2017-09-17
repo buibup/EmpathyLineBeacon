7c94b8e666956c408ee31a3485b1d51e5ce77139
buibup <b5209194@gmail.com>
2017-09-18 01:29:37 +0700

    check line beacons last enter

--- EmpathyAPI/DataAccess/MySqlConnector.cs
+++ EmpathyAPI/DataAccess/MySqlConnector.cs
@@ -10,28 +10,30 @@ using Newtonsoft.Json;
 
 namespace EmpathyLibrary.DataAccess
 {
-    public class MySqlConnector 
+    public class MySqlConnector
     {
-       
+
         public static List<SignalLineBeacons> GetSignalLineBeacons_All()
         {
             List<SignalLineBeacons> output = new List<SignalLineBeacons>();
-        
+            List<SignalLineBeacons> enters = new List<SignalLineBeacons>();
+            List<SignalLineBeacons> leaves = new List<SignalLineBeacons>();
+
             using (MySqlConnection connection = new MySqlConnection(GlobalConfig.Config["Data:ConnectionString"]))
             {
                 connection.Open();
-                using (MySqlCommand cmd = new MySqlCommand(GlobalConfig.Config["Procedure:LineBeaconCurrentDate_GetAll"], connection))
+                using (MySqlCommand cmd = new MySqlCommand(GlobalConfig.Config["Procedure:test"], connection))
                 {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    using(var reader = cmd.ExecuteReader())
+                    //cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
                     {
                         while (reader.Read())
                         {
                             var json = JsonConvert.DeserializeObject<LineBeacon>(reader[2].ToString());
-                           
-                            if(json.Type.ToString().ToLower() == "beacon")
+
+                            if (json.Type.ToString().ToLower() == "beacon")
                             {
-                                if(json.Beacon.type.ToString().ToLower() == "enter")
+                                if (json.Beacon.type.ToString().ToLower() == "enter")
                                 {
                                     var signalLineBeacons = new SignalLineBeacons()
                                     {
@@ -39,12 +41,52 @@ namespace EmpathyLibrary.DataAccess
                                         Topic = reader[1].ToString(),
                                         LineBeacon = json
                                     };
-                                    output.Add(signalLineBeacons);
+                                    enters.Add(signalLineBeacons);
+                                }
+                                else if (json.Beacon.type.ToString().ToLower() == "leave")
+                                {
+                                    var signalLineBeacons = new SignalLineBeacons()
+                                    {
+                                        Timestamp = DateTime.Parse(reader[0].ToString()),
+                                        Topic = reader[1].ToString(),
+                                        LineBeacon = json
+                                    };
+                                    leaves.Add(signalLineBeacons);
                                 }
                             }
                         }
                     }
                 }
+            }
+
+            var dataEnters = enters
+                .GroupBy(e => e.LineBeacon.Source.userId)
+                .Select(grp => new
+                {
+                    grp.Key,
+                    LastVisit = grp.OrderByDescending(
+                        x => x.Timestamp).FirstOrDefault()
+                }).ToList();
+
+
+            var dataLeves = leaves
+                .GroupBy(e => e.LineBeacon.Source.userId)
+                .Select(grp => new
+                {
+                    grp.Key,
+                    LastVisit = grp.OrderByDescending(
+                        x => x.Timestamp).FirstOrDefault()
+                }).ToList();
+
+            foreach (var dataE in dataEnters)
+            {
+                var dataL = dataLeves.Any(a => a.Key == dataE.Key) ? dataLeves.Find(a => a.Key == dataE.Key).LastVisit : new SignalLineBeacons();
+
+                if (dataE.LastVisit.Timestamp > dataL.Timestamp)
+                {
+                    output.Add(dataE.LastVisit);
+                }
+
             }
 
             output = output.OrderByDescending(x => x.Timestamp).ToList();
